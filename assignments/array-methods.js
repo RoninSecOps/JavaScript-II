a7c813fc30c5804f360d58e8db0a94d1ef75d461
Rafal Kaniewski <rafal.kaniewski@gmail.com>
2019-10-15 15:01:32 +0200

    Fixed typo

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -491,7 +491,7 @@ console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
-const fruits = ['Apple', 'Orange', 'Plum', 'Kiwi', 'Cheery', 'Banana'];
+const fruits = ['Apple', 'Orange', 'Plum', 'Kiwi', 'Cherry', 'Banana'];
 // Problem 1
 const fruitsLower = fruits.map(function(fruits) {
   return fruits.toLocaleLowerCase();
